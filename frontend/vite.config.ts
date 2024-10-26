51c2c0bd55f5d9213f98914e7290471c0441b430
mzayem <mzayemazam@gmail.com>
2024-10-27 01:00:54 +0500

    init

---
+++ frontend/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default ({ mode }: { mode: string }) => {
+  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
+
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
+    },
+    server: {
+      port: 3000, // Change to any other port you prefer
+      open: '/app'
+    }
+  });
+};
